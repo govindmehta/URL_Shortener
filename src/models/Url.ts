bbface69d2bbcbad0f6299d3a4ef615d0b77b681
Govind Mehta <govindmehta.gov@gmail.com>
2026-07-29 14:44:55 +0530

    url model added

---
+++ src/models/Url.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+export interface IUrl extends mongoose.Document {
+    originalUrl: string;
+    shortCode: string;
+    clicks: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const urlSchema: mongoose.Schema = new mongoose.Schema<IUrl>(
+    {
+        originalUrl: {
+            type: String,
+            required: true,
+        },
+        shortCode: {
+            type: String,
+            required: true,
+            unique: true,
+            index: true,
+        },
+        clicks: {
+            type: Number,
+            default: 0,
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+//automatically generate the type after updating the schema, so we don't have to manually update the type every time we change the schema
+export type Url = mongoose.InferSchemaType<typeof urlSchema>;
+
+export const Url = mongoose.model<IUrl>("Url", urlSchema);
